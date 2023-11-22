ea2af7db68aca4b6ce72b599b90453ab9926dece
Mark Barrett <markbarrett42@hotmail.com>
2023-11-22 19:34:23 +0000

    feat: Adding animation to progress bar

--- src/components/wizard/WizardProgress.tsx
+++ src/components/wizard/WizardProgress.tsx
@@ -15,7 +15,7 @@ export const ProfileWizardProgress = () => {
   return (
     <div className="w-full bg-zinc-300 h-2 rounded-md my-4 relative">
       <div
-        className="bg-red-400 absolute h-full rounded-md"
+        className="bg-red-400 absolute h-full rounded-md transition-width duration-500 ease-in-out"
         style={{ width: `${perecentageCompleted}%` }}
       ></div>
     </div>
