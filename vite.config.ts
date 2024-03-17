9fd45590151328892b992c2f39ed1fbf4ad84a66
Mark Barrett <markbarrett42@hotmail.com>
2024-03-17 14:06:06 +0000

    fix: Fixed paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "yourmove-ai",
-    project: "yourmove-ui"
-  })],
+  base: "./",
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "yourmove-ai",
+      project: "yourmove-ui",
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
