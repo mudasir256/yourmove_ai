a1c758a8359fb67a87543dcae36e4ac2aeea4377
Mark Barrett <markbarrett42@hotmail.com>
2024-03-17 13:48:17 +0000

    fix: Fixed paths

--- src/components/payment/PaymentForm.tsx
+++ src/components/payment/PaymentForm.tsx
@@ -88,7 +88,10 @@ export default function PaymentForm({
     // your `return_url`. For some payment methods like iDEAL, your customer will
     // be redirected to an intermediate site first to authorize the payment, then
     // redirected to the `return_url`.
-    if (error.type === "card_error" || error.type === "validation_error") {
+    if (
+      error &&
+      (error.type === "card_error" || error.type === "validation_error")
+    ) {
       setMessage(error.message);
     } else {
       setMessage("An unexpected error occurred.");
