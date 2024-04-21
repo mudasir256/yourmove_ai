0583ee04ecdae2d9120733b986f158e622f80995
Dmitri <damirakyan@gmail.com>
2024-04-21 18:15:30 -0400

    fixed button

--- src/components/nudges/BlaineNudge.tsx
+++ src/components/nudges/BlaineNudge.tsx
@@ -43,29 +43,29 @@ export const BlaineNudge = () => {
 export const BlaineNudgeAlways = () => {
     // only show if there are queries remaining, and they are less than 5 and the user is signed in
     return (
-              <div className="my-4 mx-2 flex flex-row items-center rounded-xl border-2 border-black 	bg-gradient-to-tr from-purple-400 via-purple-100 to-white ">
-                  <div className="basis-2/5 pr-4 pl-2 align-bottom align-text-bottom items-end">
-                      <img className="object-scale-down" src={blaine} alt="Blaine Graphic">
-                      </img>
-                  </div>
-                  <div className="basis-3/5 py-1 items-center flex-col ">
-                      <div className="pt-1">
-                          <div className="font-bold text-xs text-purple-700 md:text-base">LEARN FROM THE BEST</div>
-                          <p className="text-gray-700 text-sm md:text-lg my-1 pr-2">
-                              Get your dating life unstuck with coach Blaine→
-                          </p>
-                          </div>
-                      <div className="pb-1">
-                          <a className="inline-block bg-white text-purple-700 text-xs md:text-base text-font-bold py-1 px-2 rounded"
-                              href="https://clients.datingbyblaine.com/a/2147708898/sLdDvypd" target="_blank"
-                          >
-                          Learn More
-                      </a>
-                      </div>
-                  </div>
-              </div>
+        <a href="https://clients.datingbyblaine.com/a/2147708898/sLdDvypd" target="_blank" className="no-underline">
+            <div className="my-4 mx-2 flex flex-row items-center rounded-xl border-2 border-black bg-gradient-to-tr from-purple-400 via-purple-100 to-white ">
+                <div className="basis-2/5 pr-4 pl-2 align-bottom align-text-bottom items-end">
+                    <img className="object-scale-down" src={blaine} alt="Blaine Graphic" />
+                </div>
+                <div className="basis-3/5 py-1 items-center flex-col ">
+                    <div className="pt-1">
+                        <div className="font-bold text-xs text-purple-700 md:text-base">LEARN FROM THE BEST</div>
+                        <p className="text-gray-700 text-sm md:text-lg my-1 pr-2">
+                            Get your dating life unstuck with coach Blaine→
+                        </p>
+                    </div>
+                    <div className="pb-1">
+                        <span className="inline-block bg-white text-purple-700 text-xs md:text-base text-font-bold py-1 px-2 rounded">
+                            Learn More
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </a>
     );
-  };
+};
+
 // export const BlaineNudge = () => {
 //     const navigate = useNavigate();
 //     return (
