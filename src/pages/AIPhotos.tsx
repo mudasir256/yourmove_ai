cd0a2f0382ac9fa61b9c097bf7de6f75624a566e
Irfan Ayaz <erfan.ayaz.89@gmail.com>
2024-05-01 03:55:02 +0500

    added disclaimer text

--- src/pages/AIPhotos.tsx
+++ src/pages/AIPhotos.tsx
@@ -32,7 +32,6 @@ export const AIPhotos = () => {
 
   useEffect(() => {
     if (auth.currentUser?.email) {
-      console.log("YESSS::")
       const products = [ProductType.AIPhotos]
       // Check if the user has paid for AI Photos
       hasUserPaid(auth.currentUser?.email, products).then((response) => {
@@ -47,7 +46,6 @@ export const AIPhotos = () => {
       });
     } else {
       setShowLink(false)
-      console.log("NOOO::")
     }
   }, [authState])
 
@@ -152,54 +150,58 @@ export const AIPhotos = () => {
                 </svg>
               </div>
             ) : (
-              <button
-                type="button"
-                onClick={() => {
-                  setAIPhotosModalShow(true);
-                  if (window.gtag) {
-                    window.gtag("event", "photos_unlock", {
-                      event_category: "funnel",
-                      product: "photos",
-                    });
-                  }
-                }}
-                className="mt-2 flex items-center justify-center w-full bg-brand-primary text-white py-3 rounded-full font-semibold"
-              >
-                Enhance my photos -
-                {isSubscribed === null ? (
-                  <svg
-                    className="animate-spin ml-2 mr-2 h-5 w-5 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
-                ) : (
-                  <>
-                    {isSubscribed ? (
-                      <>
-                        <span className="line-through ml-1">$34</span>
-                        <span className="ml-1">$17 (50% discount)</span>
-                      </>
-                    ) : (
-                      "$34"
-                    )}
-                  </>
-                )}
-              </button>
+              <>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setAIPhotosModalShow(true);
+                    if (window.gtag) {
+                      window.gtag("event", "photos_unlock", {
+                        event_category: "funnel",
+                        product: "photos",
+                      });
+                    }
+                  }}
+                  className="mt-2 flex items-center justify-center w-full bg-brand-primary text-white py-3 rounded-full font-semibold"
+                >
+                  Enhance my photos -
+                  {isSubscribed === null ? (
+                    <svg
+                      className="animate-spin ml-2 mr-2 h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                  ) : (
+                    <>
+                      {isSubscribed ? (
+                        <>
+                          <span className="line-through ml-1">$34</span>
+                          <span className="ml-1">$17 (50% discount)</span>
+                        </>
+                      ) : (
+                        "$34"
+                      )}
+                    </>
+                  )}
+                </button>
+                <p className="text-sm text-slate-500 mt-2">
+                  Already purchased AI Photos? Please contact support@yourmove.ai
+                </p></>
             )
           ) : (
             <button
@@ -223,6 +225,6 @@ export const AIPhotos = () => {
           toast.success("You have successfully unlocked AI Photos!");
           setShowLink(true)
         }} />
-    </div>
+    </div >
   );
 };
